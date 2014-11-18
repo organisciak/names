c6730963577851e9bf824755dbd12e98cc0abc34
Peter Organisciak <organisciak@gmail.com>
2014-11-18 15:47:46 -0600

    Code for processing data

--- README.md
+++ README.md
@@ -1,15 +1,49 @@
 # Baby Names Datasets
 
-Baby name datasets presented in different ways. I find myself going back to this dataset frequently, this will make it easier.
+Baby name datasets presented in different ways.
+I find myself going back to this dataset frequently, and hope it is useful.
 
-## Sources
+Included are estimates of name frequencies among living people in 2014, various slices of birth name statistics since 1910, and gender probabilities.
 
-```raw/us-names-by-state.csv``` includes the social security names data for the
-USA, with number of children born within a given name, by year, sex, and 
-state. Only includes names that were given to 5+ births that year.
+## Living Citizen Name Estimates
 
-Source: 
+By cross-referencing US-born birth names with the population age distribution for 2014, I estimate the likelihood of encountering a name today.
+A number of a caveats here, most significantly that relying on baby names does not factor in the 40 million foreign-born residents in the US.
 
-```raw/us-population-by-age-and-sex-2014.csv```
+Included:
 
-Source: (Census.gov)[http://www.census.gov/population/international/data/idb/informationGateway.php]
+- `data/us-living-estimate-names-by-sex-state-year.csv`
+- `data/us-living-estimate-names-by-sex.csv`
+
+The rough estimation approach is `P(alive|age)=P(age|alive)*P(alive)/P(age)`, where `P(age|alive)` is the proportion of the current population that is the given age,
+	`P(age)` is the proportion of the baby names for year `(2014-age)` out of the full baby name set. `P(alive)` would be the percent of people born since 1910 that
+	is still alive, but this is hard to estimate. Instead, I make a guess based on `P(alive|age=1)` being close to 1.0 (infant mortality rates gives us a real
+	estimate, 0.9939). This would put the sum US population since 1910 at about 1.05 billion. Regardless, since all this is rough, _it's best to consider the
+	ranking more than the actual counts_.
+
+## Gender Likelihood
+
+Guesses for a gender based on a first name, in 2014. Includes birth year so you can filter to population age groups.
+
+`data/us-likelihood-of-gender-by-name-in-2014.csv`
+
+## Most popular US-born baby names since 1910
+
+`raw/us-names-by-gender-state-year.csv` -- the source data, of name counts per gender, year, and state, since 1910.
+Source: Social Security Administration
+
+- `data/us-names-by-gender.csv` - most popular names, overall
+- `data/us-names-by-year.csv` - most popular names by year
+- `data/us-names-by-decade.csv` - most popular names by decade
+- `data/us-names-by-state.csv` - most popular names by state, overall
+
+## Other
+
+- `lists/top-us-female-names.csv, lists/top-us-male-names.csv` - Truncated list of most common US names.
+- `lists/top-us-[male|female]-names-alive-in-2014.csv` - 1000 most common names in 2014.
+- `lists/us-50-gender-neutral-names.csv` - 50 fairly common gender-neutral names in 2014.
+- `lists/us-dead-names.csv` - List of names that are dying out among the US-born population.
+
+## Other sources
+
+- `raw/us-population-by-age-and-sex-2014.csv` - Population age distributions. Source: [Census.gov](http://www.census.gov/population/international/data/idb/informationGateway.php)
---
+++ scripts/process-baby-names.R
@@ -0,0 +1,75 @@
+library("data.table")
+
+# Import name counts by state, sex, year
+names <- fread("raw/us-names-by-gender-state-year.csv")
+
+## Save name counts by sex
+tmp <- names[, list(count=sum(count)), by=list(sex, name)][order(-count)]
+write.csv(tmp, "data/us-names-by-gender.csv", quote=F, row.names=F)
+
+## Smaller data:top 1000 names by sex
+tmp <- names[sex=='F', list(count=sum(count)), by=list(sex,name)][order(-count)][1:1000]
+write.csv(tmp[,list(name)], "lists/top-us-female-names.csv", quote=F, row.names=F)
+tmp <- names[sex=='M', list(count=sum(count)), by=list(sex,name)][order(-count)][1:1000]
+write.csv(tmp[,list(name)], "lists/top-us-male-names.csv", quote=F, row.names=F)
+
+
+## Save name counts by decade
+## Sorting by decade is newest-oldest
+names[,decade:=year-(year %% 10),]
+tmp <- names[, list(count=sum(count)), by=list(sex, name, decade)][order(-decade, -count)]
+write.csv(tmp, "data/us-names-by-decade.csv", quote=F, row.names=F)
+
+## Estimate name popularity order in 2014 by accounting for life expectancy
+## 
+## P(alive|age)=P(age|alive)*P(alive)/P(age)
+## where,
+## P(age|alive) is the percentage of the 2014 population that is that age
+## P(alive) is the current population divided by total American count.
+##      This is difficult to estimate (births+unique immigrants, but easier
+##      said than done), so currently, is ignored. Thus the COUNTS ARE NOT MEANINGFUL.
+## P(age) is the percent of baby names at that age relative to all baby names.
+
+ages <- fread("raw/us-population-by-age-and-sex-2014.csv")
+names[, age:=as.factor(2014-year),]
+names[age %in% as.character(100:104), age:="100+"]
+names[age %in% as.character(95:99), age:="95-99"]
+names[age %in% as.character(90:94), age:="90-94"]
+names[age %in% as.character(85:89), age:="85-89"]
+alive <- names[, list(count.in.names=sum(count)), by=list(sex, age)][,percent.of.names:=100*count.in.names/sum(count.in.names)]
+ages <- merge(alive, ages, by=c("sex", "age"))
+ages[,modifier:=percent/percent.of.names,]
+# Rough P(alive) estimate, assuming most 1 year olds are alive
+# In fact, we know h0w many don't survive by 1 year, 6.17/1000, 
+# but this would lend a false sense of precision
+ages[, percent.alive:=modifier/modifier[1], by=list(sex)]
+tmp <- merge(names, ages[,list(sex, age, percent.alive)], by=c("sex", "age"))
+tmp[,alive.count:=round(count*percent.alive)]
+names.in.2014 <- tmp[,percent.alive:=NULL]
+rm(ages, alive, tmp)
+## Save Living estimates per year
+write.csv(names.in.2014[alive.count>0, list(state, sex, year, name, alive.count)], "data/us-living-estimate-names-by-sex-state-year.csv", quote=F, row.names=F)
+
+## Save living estimate by gender/name. More useful.
+tmp <- names.in.2014[, list(count=sum(alive.count)), by=list(sex, name)][order(-count)]
+write.csv(tmp[count > 10], 
+          "data/us-living-estimate-names-by-sex.csv", 
+          quote=F, row.names=F)
+write.csv(tmp[sex=='M'][1:1000][,list(name)], "lists/top-us-male-names-alive-in-2014.csv", quote=F, row.names=F)
+write.csv(tmp[sex=='F'][1:1000][,list(name)], "lists/top-us-female-names-alive-in-2014.csv", quote=F, row.names=F)
+
+
+## Save likelihood of name gender
+tmp[,gender.prob:=count/sum(count), by=name]
+write.csv(tmp[gender.prob > 0.5 & count > 100][order(-gender.prob, -count)][,list(sex, name, gender.prob)], 
+          "data/us-likelihood-of-gender-by-name-in-2014.csv",
+          quote=F, row.names=F)
+
+## For fun: gender-neutral names that are fairly common in 2014 in the US
+write.csv(tmp[gender.prob > 0.5 & count > 1000][order(gender.prob)][1:50][, list(name, sex, gender.prob)], 
+          "lists/us-50-gender-neutral-names.csv", 
+          quote=F, row.names=F)
+
+## For fun: Names for US-born people that are all but dead
+tmp <- names.in.2014[, list(name.survival=sum(alive.count)/sum(count)), by=list(sex, name)][name.survival > 0][order(name.survival)][1:1000][,name]
+write.csv(tmp, "lists/us-dead-names.csv", quote=F, row.names=F)
